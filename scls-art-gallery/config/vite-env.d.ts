79dc40505e2535bfd2906b079d0c4d898d3644fd
E_Berlin <trondomaunity@outlook>
2025-11-19 09:43:50 +0800

    Fix TS build errors & import.meta.env

--- scls-art-gallery/config/vite-env.d.ts
+++ scls-art-gallery/config/vite-env.d.ts
@@ -2,13 +2,13 @@
 /// <reference types="vite/client" />
 
 declare global {
-  interface ImportMetaEnv {
-      readonly VITE_SERVER_URL: string;
-      readonly VITE_FRONTEND_URL: string;
-      readonly VITE_SUPABASE_URL: string;
-  }
+    interface ImportMetaEnv {
+        readonly VITE_SERVER_URL: string;
+        readonly VITE_FRONTEND_URL: string;
+        readonly VITE_SUPABASE_URL: string;
+    }
 
-  interface ImportMeta {
-      readonly env: ImportMetaEnv;
-  }
+    interface ImportMeta {
+        readonly env: ImportMetaEnv;
+    }
 }
